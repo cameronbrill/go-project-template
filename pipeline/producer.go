7de0dc369fd92012c56adc912707f6e1fae9507f
Cameron Brill <c00brill@gmail.com>
2022-04-18 21:47:16 -0700

    fix race condition

--- pipeline/producer.go
+++ pipeline/producer.go
@@ -9,6 +9,9 @@ func Producer[T any](ctx context.Context, input []T) <-chan T {
 
 	go func() {
 		defer close(outChannel)
+		if err := ctx.Err(); err != nil {
+			return
+		}
 
 		for _, v := range input {
 			select {
